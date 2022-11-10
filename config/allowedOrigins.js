2e7e1372072bed8ed88a9d1ba0ef73d81a1879cd
Joey3011 <jeff.me.7919@gmail.com>
2022-11-11 05:02:09 +0800

    update commit Exercise Tracker

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3500',
-    'https://www.freecodecamp.org'
+    'https://www.freecodecamp.org',
+    'https://PerfumedGuiltyExternalcommand.joeyhipolito.repl.co'
 ]
 
 module.exports = allowedOrigins
