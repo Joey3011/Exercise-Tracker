cbf4cf39785af2cfa896d44c203994568c3ea95b
Joey3011 <jeff.me.7919@gmail.com>
2022-11-11 16:41:50 +0800

    minor update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
-    'http://localhost:3500',
-    'https://www.freecodecamp.org',
-    'https://PerfumedGuiltyExternalcommand.joeyhipolito.repl.co'
+    'http://localhost:3000',
+    'https://www.freecodecamp.org'
 ]
 
 module.exports = allowedOrigins
